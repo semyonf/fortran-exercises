f35f27b8d33ee2ba708848dee0e5eeafdbc8d326
Semyon Fomin <fs@get8.ru>
2016-10-19 19:31:58 +0300

    Finish ex_3_20

--- ex_3_20/src/ex_3_20.f90
+++ ex_3_20/src/ex_3_20.f90
@@ -2,19 +2,30 @@
 
 program ex_3_20
     implicit none
-    integer, parameter :: arrayX = 2, arrayY = 3
-    integer, dimension(arrayX, arrayY) :: A
-    integer, dimension(arrayX * arrayY) :: B, C
-    integer :: i, j, k = 1
+    integer, parameter      :: arrayX = 3, arrayY = 2
+    integer                 :: A(arrayX, arrayY)
+    integer                 :: B(arrayX * arrayY), C(arrayX * arrayY)
+    integer                 :: i, j, Out = 0
+    character(*), parameter :: output_file = "output.txt", &
+                               E_ = "UTF-8"
 
     A = reshape((/ 1, 2, 3, 4, 5, 6 /), shape(A))
 
-    do j = 1, arrayY
+    B = reshape(A, shape(B))
+    C = reshape(transpose(A), shape(C))
+
+    open (file=output_file, encoding=E_, newunit=Out)
+        write(Out,*) 'A'
+        write(Out,*)
         do i = 1, arrayX
-            B(k) = A(i,j)
-            C(k) = A(j,i)
-            k = k + 1
+                write(Out,"(10g3.5)") ( A(i,j), j=1, arrayY)
         enddo
-    enddo
+        write(Out,*) 'B'
+        write(Out,*)
+        write(Out,"(10g3.5)") ( B(i), i=1, arrayY*arrayX)
+        write(Out,*) 'C'
+        write(Out,*)
+        write(Out,"(10g3.5)") ( C(i), i=1, arrayY*arrayX)
+    close (Out)
 
 end program ex_3_20
