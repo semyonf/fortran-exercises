d007d34790017cd8b27243bb666ecdf294e08a65
Semyon Fomin <fs@get8.ru>
2016-10-19 23:09:19 +0300

    Read array from file for ex_7_19a

--- ex_7_19a/src/7.19a.f90
+++ ex_7_19a/src/7.19a.f90
@@ -7,14 +7,19 @@ program ex_7_19a
     integer, parameter      :: x = 4, y = 5
     integer                 :: B(x, y)
     logical                 :: mask(x, y)
-    integer                 :: i, j, positiveElements = 0, Out = 0
+    integer                 :: i, j, positiveElements = 0, Out = 0, In = 0
     character(*), parameter :: output_file = "output.txt", &
+                               input_file = "../data/input.txt", &
                                E_ = "UTF-8"
 
-    B = reshape((/ 1, 2, -3, 4, 5, 6, 7, 8, -9, 10, &
-                   11, 0, -13, 14, 15, 16, -17, 18, 19, 20/), shape(B))
+    open (file=input_file, encoding=E_, newunit=In)
+        do i = 1, y
+            read(In,"(10g3.5)") (B(j, i), j = 1, x)
+        enddo
+    close (In)
 
     mask = B .GE. 0
+    positiveElements = count(mask)
 
     open (file=output_file, encoding=E_, newunit=Out)
         write(Out,*) 'B'
@@ -22,8 +27,6 @@ program ex_7_19a
             write(Out,"(10g3.5)") (B(j, i), j = 1, x)
         enddo
         write(Out,*)
-        positiveElements = count(mask)
         write(Out,*) 'Positive elements:', positiveElements
     close (Out)
-
 end program ex_7_19a
