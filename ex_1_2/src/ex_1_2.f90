2e44ed50abedca64ab3ce403d59d617d47df29c2
Semyon Fomin <fs@get8.ru>
2016-10-22 15:53:06 +0300

    Fix some minor stuff

--- ex_1_2/src/ex_1_2.f90
+++ ex_1_2/src/ex_1_2.f90
@@ -3,20 +3,20 @@
 program ex_1_2
     implicit none
 
-    integer, parameter      :: R_ = 4
+    integer, parameter      :: R_ = 4, L = 6
     character(*), parameter :: input_file = "../data/input.txt", &
                                output_file = "output.txt", &
                                E_ = "UTF-8"
     integer                 :: Out = 0, In = 0, i
-    real(R_)                :: vals(6), xs(6), x
+    real(R_)                :: vals(L), xs(L), x
 
     open (file=input_file, encoding=E_, newunit=In)
         read(In,'(6f7.2 )') vals
         read(In,'(f7.2 )') x
     close (In)
 
-    forall (i=0:6)
-        xs(6-i) = x ** i
+    forall (i=0:L)
+        xs(L-i) = x ** i
     end forall
 
     open (file=output_file, encoding=E_, newunit=Out)
