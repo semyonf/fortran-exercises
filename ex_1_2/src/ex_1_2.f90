68b40863a8fcb9725a3bb390472d0b40a4cd77ae
Semyon Fomin <fs@get8.ru>
2016-10-26 01:44:35 +0300

    Fix read format

--- ex_1_2/src/ex_1_2.f90
+++ ex_1_2/src/ex_1_2.f90
@@ -3,20 +3,20 @@
 program ex_1_2
     implicit none
 
-    integer, parameter      :: R_ = 4, L = 6
+    integer, parameter      :: R_ = 4, L_ = 6
     character(*), parameter :: input_file = "../data/input.txt", &
                                output_file = "output.txt", &
                                E_ = "UTF-8"
     integer                 :: Out = 0, In = 0, i
-    real(R_)                :: vals(L), xs(L), x
+    real(R_)                :: vals(L_), xs(L_), x
 
     open (file=input_file, encoding=E_, newunit=In)
-        read(In,'(6f7.2 )') vals
+        read(In,'(6f5.2 )') vals
         read(In,'(f7.2 )') x
     close (In)
 
-    forall (i=0:L)
-        xs(L-i) = x ** i
+    forall (i=0:L_)
+        xs(L_-i) = x ** i
     end forall
 
     open (file=output_file, encoding=E_, newunit=Out)
