135e7988059b2d27e188df1b0f8f0e8df6d33e14
Semyon Fomin <fs@get8.ru>
2016-10-26 10:32:54 +0300

    Add named constant

--- ex_4_1v/src/ex_4_1v.f90
+++ ex_4_1v/src/ex_4_1v.f90
@@ -3,24 +3,25 @@
 program ex_4_1v
     implicit none
 
+    integer, parameter      :: total = 1000
     real                    :: x = .01254, delta = .0002, f
-    real                    :: arNumbers(1000)
+    real                    :: arNumbers(total)
     character(*), parameter :: output_file = "output.txt", &
                                E_ = "UTF-8"
     integer                 :: Out = 0, i = 0
 
-    do i = 1, 1000
+    do i = 1, total
         arNumbers(i) = x
         x = x + delta
     enddo
 
-    do i = 1, 1000
+    do i = 1, total
         f = arNumbers(i)
         arNumbers(i) = f**2 * TAN(f) + (SIN(f)/f)
     enddo
 
     open (file=output_file, encoding=E_, newunit=Out)
-        do i = 1, 1000
+        do i = 1, total
             write(Out,'(f5.3)') arNumbers(i)
         enddo
     close (Out)
