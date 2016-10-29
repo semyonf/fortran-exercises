4f20d82b3f371b2803a8da49e324e965e34f1d4c
Semyon Fomin <fs@get8.ru>
2016-10-29 17:48:28 +0300

    Make array allocatable and read size from file

--- ex_2_3/src/ex_2_3.f90
+++ ex_2_3/src/ex_2_3.f90
@@ -3,26 +3,26 @@
 program ex_2_3
     implicit none
 
+        integer                 :: Out = 0, In = 0, result = 0, quantity = 0
         integer, parameter      :: R_ = 4
-        integer(1), parameter   :: quantity = 4
-        integer(1)              :: i
-        logical                 :: values(quantity)
-        real(R_)                :: value
+        logical, allocatable    :: mask(:)
+        real(R_), allocatable   :: values(:)
         character(*), parameter :: input_file = "../data/input.txt", &
                                    output_file = "output.txt", &
                                    E_ = "UTF-8"
-        integer                 :: Out = 0, In = 0, result = 0
 
         open (file=input_file, encoding=E_, newunit=In)
-            do i = 1, quantity
-                read(In,'(f5.2)') value
-                if (value > 0) then
-                    values(i) = .TRUE.
-                endif
-            end do
+            read(In,'(I2)') quantity
+
+            allocate(mask(quantity))
+            allocate(values(quantity))
+
+            read(In, '(f5.2)') values(:)
+            write(*,*) values
         close (In)
 
-        result = count(values);
+        mask = values >= 0
+        result = count(mask);
 
         open (file=output_file, encoding=E_, newunit=Out)
             write(Out,'(I3)') result
