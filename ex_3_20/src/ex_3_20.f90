71c69cebc622b79904ba7cf0fb1a01d5e46a4fac
Semyon Fomin <fs@get8.ru>
2016-10-30 16:35:32 +0300

    Update 3_20

--- ex_3_20/src/ex_3_20.f90
+++ ex_3_20/src/ex_3_20.f90
@@ -2,36 +2,38 @@
 
 program ex_3_20
     implicit none
-    integer, parameter      :: arrayX = 3, arrayY = 2
-    integer                 :: A(arrayX, arrayY)
-    integer                 :: B(arrayX * arrayY), C(arrayX * arrayY)
-    integer                 :: i, j, Out = 0, In = 0
+    integer                 :: arrayX, arrayY, Out = 0, In = 0
+    integer, allocatable    :: input(:), A(:,:), B(:), C(:)
     character(*), parameter :: output_file = "output.txt", &
                                input_file = "../data/input.txt", &
                                E_ = "UTF-8"
 
-    23 format(2(/,3i3))
-    6 format(6i3)
-
     open (file=input_file, encoding=E_, newunit=In)
-        do i = 1, arrayY
-            read(In,"(10g3.5)") (A(j, i), j = 1, arrayX)
-        enddo
+        read(In, '(1I3)') arrayX
+        read(In, '(1I3)') arrayY
+
+        allocate(input(arrayY * arrayX))
+
+        allocate(A(arrayY, arrayX))
+        allocate(B(arrayY * arrayX))
+        allocate(C(arrayY * arrayX))
+
+        read(In,'(I2)') input(:)
+
     close (In)
 
-    B = reshape(A, shape(B))
-    C = reshape(transpose(A), shape(C))
+    A = reshape(input, [arrayX, arrayY])
+    C = reshape(A, [arrayX * arrayY])
+    A = reshape(input, [arrayX, arrayY], order=[2, 1])
+    B = reshape(A, [arrayX * arrayY])
 
     open (file=output_file, encoding=E_, newunit=Out)
         write(Out,*) 'A'
-        write(Out,*)
-        write(Out, 23) A
+        write(Out,'(:(/,3i3))') A
         write(Out,*) 'B'
-        write(Out,*)
-        write(Out, 6) B
+        write(Out,'(I3)') B(:)
         write(Out,*) 'C'
-        write(Out,*)
-        write(Out, 6) C
+        write(Out,'(I3)') C(:)
     close (Out)
 
 end program ex_3_20
