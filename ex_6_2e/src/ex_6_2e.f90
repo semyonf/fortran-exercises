79486f768da84d4b9cd4a5ffc2f42d21365e5146
Semyon Fomin <fs@get8.ru>
2016-10-30 17:00:07 +0300

    Fix 6_2e

--- ex_6_2e/src/ex_6_2e.f90
+++ ex_6_2e/src/ex_6_2e.f90
@@ -4,28 +4,23 @@ program ex_6_2e
     implicit none
 
     integer, parameter      :: R_ = 4
-    real(R_), parameter     :: RELERR = 0.0005
+    real(R_), parameter     :: RELERR = 0.005
     character(*), parameter :: output_file = "output.txt", &
                                input_file = "../data/input.txt", &
                                E_ = "UTF-8"
-    real(R_)                :: x = 0, sum = 0, current = 0, a = 0
-    integer                 :: In = 0, Out = 0, i = 0, factI = 1, k =0
+    real(R_)                :: x = 0, sum = 0, current = 1, a = 0
+    integer                 :: In = 0, Out = 0, i = 1
 
     open (file=input_file, encoding=E_, newunit=In)
-        read(In,'(1f5.2)') a
-        read(In,'(1f5.2)') x
+        read(In,'(f5.2)') a
+        read(In,'(f5.2)') x
     close (In)
 
     do
-        factI = 1
-        do k = 1, i
-            factI = factI * k
-        enddo
-
-        current = (x * log(a)) ** i / factI
         sum = sum + current
-        ! write(*,*) 'current = ', current, 'sum = ', sum
-        if ((current / sum) .LT. RELERR) then
+        write(*,*) 'current = ', current, 'sum = ', sum
+        current = current * x * log(a) / i
+        if ((current / sum) < RELERR) then
             exit
         endif
         i = i + 1
