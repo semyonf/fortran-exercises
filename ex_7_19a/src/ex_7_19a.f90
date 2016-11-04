bd59c41977e89c18eff80d8afdef1eb39cdd968c
Semyon Fomin <fs@get8.ru>
2016-11-04 14:34:37 +0300

    Fix ex_7_19a

--- ex_7_19a/src/ex_7_19a.f90
+++ ex_7_19a/src/ex_7_19a.f90
@@ -1,33 +1,53 @@
 ! 7.19a в учебнике
 ! количесвто элементов матрицы B, которые положительны
+! Корректно читать двумерный массив по строкам.
+! "Count. Составить маску. Упаковать массив индексов (см. раздаточные 7.х)."
 
 program ex_7_19a
     implicit none
-
-    integer                 :: x, y, Positives = 0, Out = 0, In = 0
-    integer, allocatable    :: B(:,:), input(:)
+    integer, allocatable    :: B(:,:), Indexes(:,:), NegativeIndexes(:)
     logical, allocatable    :: mask(:,:)
+    integer                 :: x = 0, y = 0, In = 1, Out = 0, i = 0, j = 0, positives = 0
     character(*), parameter :: output_file = "output.txt", &
-                               input_file = "../data/input.txt", &
-                               E_ = "UTF-8"
+                               input_file = "../data/input.txt"
+    character(50)           :: fmt
+    character(50)           :: temp
 
-    open (file=input_file, encoding=E_, newunit=In)
-        read(In,'(I3)') x
-        read(In,'(I3)') y
+    open (file=input_file, newunit=In)
+        read(In, *) x, y
 
-        allocate(input(x * y))
-        allocate(mask(y,x))
-        allocate(B(y,x))
+        allocate(B(x,y), mask(x,y), Indexes(x,y))
 
-        read(In,'(I3)') input(:)
+        read(In, *) (B(:,i), i = 1, y)
     close (In)
 
-    B = reshape(input, shape=[y, x])
+    mask = B > 0
+    positives = count(mask)
 
-    mask = B >= 0
-    Positives = count(mask)
+    allocate(NegativeIndexes(count(.NOT. mask)))
 
-    open (file=output_file, encoding=E_, newunit=Out)
-        write(Out,*) 'Positive elements:', Positives
-    close (Out)
+    do i = 1, y
+        do j = 1,x
+            Indexes(j,i) = i*10 + j
+        enddo
+    enddo
+
+    NegativeIndexes = pack(Indexes, .NOT. mask)
+
+    ! Array looks cleaner this way
+    write(temp, *) x
+    fmt = '(' // trim(temp) // 'I4)'
+
+    open (file=output_file, newunit=Out)
+        write(Out, *) 'Array B'
+        write(Out, fmt) (B(:,i), i = 1, y)
+        write(Out, *)
+        write(Out, *) 'Array Indexes'
+        write(Out, fmt) (Indexes(:,i), i = 1, y)
+        write(Out, *)
+        write(Out, *) NegativeIndexes, ' - negative indexes'
+        write(Out, *)
+        write(Out, *) positives, ' positive elements within array found'
+    close (In)
+
 end program ex_7_19a
