f052ed1c6c295c2348006b465231bb75ccd134ba
Semyon Fomin <fs@get8.ru>
2016-11-04 14:52:28 +0300

    Better algorithm

--- ex_6_2e/src/ex_6_2e.f90
+++ ex_6_2e/src/ex_6_2e.f90
@@ -4,30 +4,29 @@ program ex_6_2e
     implicit none
 
     integer, parameter      :: R_ = 4
-    real(R_), parameter     :: RELERR = 0.005
     character(*), parameter :: output_file = "output.txt", &
-                               input_file = "../data/input.txt", &
-                               E_ = "UTF-8"
-    real(R_)                :: x = 0, sum = 0, current = 1, a = 0
-    integer                 :: In = 0, Out = 0, i = 1
+                               input_file = "../data/input.txt"
+    real(R_)                :: x = 0, a = 0, sum = 0, current = 0, new = 0
+    integer                 :: In = 0, Out = 0, i = 0
 
-    open (file=input_file, encoding=E_, newunit=In)
+    open (file=input_file, newunit=In)
         read(In,'(f5.2)') a
         read(In,'(f5.2)') x
     close (In)
 
+    current = 1
+
     do
-        sum = sum + current
-        write(*,*) 'current = ', current, 'sum = ', sum
-        current = current * x * log(a) / i
-        if ((current / sum) < RELERR) then
-            exit
-        endif
         i = i + 1
+        sum = sum + current
+        new = current * x * log(a) / i
+        if (new == current) &
+            exit
+        current = new
     end do
 
-    open (file=output_file, encoding=E_, newunit=Out)
-        write(Out, *) sum
+    open (file=output_file, newunit=Out)
+        write(Out, *) sum, ' ~ ' ,a**x
     close (Out)
 
 end program ex_6_2e
