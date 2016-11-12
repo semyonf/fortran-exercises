d3fe6bd6334d9779a9db4ba59f13a86f76774397
Semyon Fomin <fs@get8.ru>
2016-11-12 19:02:32 +0300

    Everything in one array

--- ex_7_2a/src/ex_7_2a.f90
+++ ex_7_2a/src/ex_7_2a.f90
@@ -4,7 +4,7 @@ program ex_7_2a
     implicit none
 
     integer                 :: size = 0, negatives = 0, Out = 0, In = 0, i = 0, j = 0, t = 0
-    integer, allocatable    :: Neg(:), Pos(:), Arr(:), Res(:)
+    integer, allocatable    :: Arr(:)
     logical, allocatable    :: mask(:)
     character(*), parameter :: output_file = "output.txt", &
                                input_file = "../data/input.txt"
@@ -16,26 +16,23 @@ program ex_7_2a
     close (In)
 
     mask = Arr <= 0
-    negatives = count(mask)
 
-    Neg = pack(Arr, mask)
-    Pos = pack(Arr, .NOT. mask)
+    negatives = count(mask)
+    Arr = [pack(Arr, mask), pack(Arr, .not. mask)]
 
     ! Сортировка
     do i = 1, negatives
         do j = 1, i
-            if (Neg(j) > Neg(j+1)) then
-                t = Neg(j)
-                Neg(j) = Neg(j+1)
-                Neg(j+1) = t
+            if (Arr(j) > Arr(j+1)) then
+                t = Arr(j)
+                Arr(j) = Arr(j+1)
+                Arr(j+1) = t
             endif
         enddo
     enddo
 
-    Res = [pack(Neg, .true.), pack(Pos, .true.)]
-
     open (file=output_file, newunit=Out)
-        write(Out, *) Res(:)
+        write(Out, *) Arr(:)
     close (Out)
     
 end program ex_7_2a
