c11b86b235b6a61d25ce02078058e34bfce6a63a
Semyon Fomin <fs@get8.ru>
2016-11-14 15:35:03 +0300

    Concurrent and trickier sorting

--- ex_7_32g/src/ex_7_32g.f90
+++ ex_7_32g/src/ex_7_32g.f90
@@ -4,8 +4,8 @@
 program ex_7_32g
     implicit none
 
-    integer, allocatable    :: B(:,:), line(:)
-    integer                 :: In, Out, x, y, i, j, t, k
+    integer, allocatable    :: B(:,:), Line(:), Order(:)
+    integer                 :: In, Out, x, y, i, j, shiftAmount
 
     character(*), parameter :: output_file = "output.txt", &
                                input_file = "../data/input.txt"
@@ -13,22 +13,20 @@ program ex_7_32g
     open (file=input_file, newunit=In)
         read(In, *) x, y
         allocate(B(x,y))
+        allocate(Order(x))
         read(In, *) (B(:,i), i = 1, y)
     close (In)
 
-    do j = 1, y
-        line = B(:,j)
+    do concurrent (j = 1:y)
+        Line = abs(B(:,j))
+        Order = [(i,i = 1, x)]
         do i = 1, x
-            do k = i + 1, x
-                if (abs(line(i)) > abs(line(k))) then
-                    t = line(k)
-                    line(k) = line(i)
-                    line(i) = t
-                endif
-            enddo
+            shiftAmount = minloc(Line(i:), 1) - 1
+            Line(i:)  = cshift(Line(i:), shiftAmount)
+            Order(i:) = cshift(Order(i:), shiftAmount)
         enddo
-        B(:,j) = line
-    enddo
+        B(:,j) = B(order,j)
+    end do
 
     open (file=output_file, newunit=Out)
         write(Out, *) (B(:,j), j = 1, y)
