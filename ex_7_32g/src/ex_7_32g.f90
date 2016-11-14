b57f47977b3af0080024c5d86e8a96edf97a54ce
Semyon Fomin <fs@get8.ru>
2016-11-14 15:55:19 +0300

    Shorter code

--- ex_7_32g/src/ex_7_32g.f90
+++ ex_7_32g/src/ex_7_32g.f90
@@ -4,7 +4,7 @@
 program ex_7_32g
     implicit none
 
-    integer, allocatable    :: B(:,:), Line(:), Order(:)
+    integer, allocatable    :: B(:,:), AbsLine(:), Order(:)
     integer                 :: In, Out, x, y, i, j, shiftAmount
 
     character(*), parameter :: output_file = "output.txt", &
@@ -12,17 +12,16 @@ program ex_7_32g
 
     open (file=input_file, newunit=In)
         read(In, *) x, y
-        allocate(B(x,y))
-        allocate(Order(x))
+        allocate(B(x,y), Order(x))
         read(In, *) (B(:,i), i = 1, y)
     close (In)
 
     do concurrent (j = 1:y)
-        Line = abs(B(:,j))
+        AbsLine = abs(B(:,j))
         Order = [(i,i = 1, x)]
         do i = 1, x
-            shiftAmount = minloc(Line(i:), 1) - 1
-            Line(i:)  = cshift(Line(i:), shiftAmount)
+            shiftAmount = minloc(AbsLine(i:), 1) - 1
+            AbsLine(i:)  = cshift(AbsLine(i:), shiftAmount)
             Order(i:) = cshift(Order(i:), shiftAmount)
         enddo
         B(:,j) = B(order,j)
