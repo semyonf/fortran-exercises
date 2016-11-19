e974f9ded7416976d9bb88bff690238293164844
Semyon Fomin <fs@get8.ru>
2016-11-19 19:25:07 +0300

    Sorta fixed ex_6_2e

--- ex_6_2e/src/ex_6_2e.f90
+++ ex_6_2e/src/ex_6_2e.f90
@@ -3,11 +3,11 @@
 program ex_6_2e
     implicit none
 
-    integer, parameter      :: R_ = 4
+    integer, parameter      :: R_ = 16
     character(*), parameter :: output_file = "output.txt", &
                                input_file = "../data/input.txt"
-    real(R_)                :: x = 0, a = 0, currentElement = 0, newElement = 0, currentSum = 0, newSum = 0
-    integer                 :: In = 0, Out = 0, i = 0
+    real(R_)                :: x = 0, a = 0, currentElement = 0, newElement = 0, currentSum = 0, newSum = 0, numerator = 0
+    integer                 :: In = 0, Out = 0, denominator = 0
 
     open (file=input_file, newunit=In)
         read(In,'(f5.2)') a
@@ -17,19 +17,17 @@ program ex_6_2e
     currentElement = 1
 
     do
-        i = i + 1
+        denominator = denominator + 1
         currentSum = newSum
         newSum = currentSum + currentElement
-
-        newElement = currentElement * x * log(a) / i
+        numerator = x * log(a)
+        newElement = currentElement * numerator / denominator
         currentElement = newElement
-
-        if (currentSum == newSum) &
-            exit
+        if (currentSum == newSum) exit
     end do
 
     open (file=output_file, newunit=Out)
-        write(Out, *) currentSum, ' ~ ' , a**x
+        write(Out, '(f5.2,a,f5.2)') currentSum, ' ~ ' , a**x
     close (Out)
 
 end program ex_6_2e
