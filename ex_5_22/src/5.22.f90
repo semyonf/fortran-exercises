a0092829a6ec24192c3d0c903841849b4ec9b84d
Semyon Fomin <fs@get8.ru>
2016-11-21 02:07:44 +0300

    Finish ex_5_22

--- ex_5_22/src/5.22.f90
+++ ex_5_22/src/5.22.f90
@@ -1,16 +1,19 @@
 ! 5.22 в учебнике
 
 real function F(x)
+
     implicit none
     real :: x
+
     F = x ** 2
+
 end function F
 
 program ex_5_22
     implicit none
     integer, parameter      :: R_ = 4
     integer                 :: Out = 0, In = 0, i = 0, N = 0
-    real(R_)                :: h = .0, s = .0, t = .0, a = 0, b = 0, F
+    real(R_)                :: h = .0, sum = .0, a = 0, b = 0, x = .0, integral = .0, F
     character(*), parameter :: output_file = "output.txt", &
                                input_file  = "../data/input.txt"
 
@@ -19,22 +22,33 @@ program ex_5_22
         read(In, *) N
     close (In)
 
-    h = (b - a) / real(N)
-    s = 0
+    if (mod(N,2) == 1 .or. N <= 0) then
+        write(*,*) 'Число N должно быть положительным и четным. Отмена...'
+        stop
+    endif
 
-    t = a + h
+    h = (b - a) / N
+    x = a
+    sum = F(x)
 
-    do i = 1, N
-        s = s + F(a) + 4 * F(a + h/2) + f(t)
-        a = a + h
-        t = t + h
+    i = 1
+    do
+        x = x + h
+        sum = sum + 4 * F(x)
 
+        i = i + 2
+
+        if (i >= N) exit
+
+        x = x + h
+        sum = sum + 2 * F(x)
     enddo
 
-    s = h / 6 * s
+    sum = sum + F(b)
+    integral = h/3 * sum
 
     open (file=output_file, newunit=Out)
-        write(Out,*) 'Интеграл ', s
+        write(Out,*) 'Интеграл ', integral
     close (Out)
 
 end program ex_5_22
