904a07044bb31eb89e0533329bbee87c9032eec2
Semyon Fomin <fs@get8.ru>
2016-11-21 02:47:40 +0300

    Finish ex_8_24

--- ex_8_24/src/ex_8_24.f90
+++ ex_8_24/src/ex_8_24.f90
@@ -2,49 +2,81 @@
 
 program ex_8_24
     implicit none
-
-    integer                 :: B(2,3), i, j, In, Out
+    integer, parameter      :: R_ = 4
+    integer                 :: In = 0, Out = 0, N = 0, i = 0
+    real(R_)                :: x = .0, a = .0, b = .0, h = .0, sum = .0, integral = .0, t = .0
     character(*), parameter :: output_file = "output.txt", &
-                               input_file = "../data/input.txt", &
-                               E_ = "UTF-8"
+                               input_file = "../data/input.txt"
+
+    open (file=input_file, newunit=In)
+        read(In, *) a, b
+        read(In, *) N
+        read(In, *) x
+    close (In)
+
+    if (mod(N,2) == 1 .or. N <= 0) then
+        write(*,*) 'Число N должно быть положительным и четным. Отмена...'
+        stop
+    endif
+
+    h = (b - a) / N
+    t = a
+    sum = F(t)
+
+    i = 1
+    do
+        t = t + h
+        sum = sum + 4 * F(t)
+
+        i = i + 2
+
+        if (i >= N) exit
+
+        t = t + h
+        sum = sum + 2 * F(t)
+    enddo
+
+    sum = sum + F(b)
+    integral = h/3 * sum
+
+    open (file=output_file, newunit=Out)
+        write(Out,*) 'Ответ ', integral
+    close (Out)
 
 contains
+
+real function F(x)
+    implicit none
+    real, intent(in) :: x
+
+    F = (q(x) ** 2 + 1.57) / sqrt(q(x) + 2.15)
+    
+end function F
 
 pure real function q(x)
     implicit none
     real, intent(in) :: x
-    real             :: sum, current
-    integer          :: i, k
+    real             :: sum, old_E, new_E
+    integer          :: i, j
 
-    i = 0
-    k = 1
+    sum   = 1
+    new_E = 1
 
+    i   = 1
     do
-        current = (x ** i) / fac(k)
-        sum = sum + current
-        if (current .LE. (10 ** (-5))) then
-            exit
-        endif
-        i = i + 1
-        k = k + 2
+        old_E = new_E
+        new_E = old_E * x**i
+
+        do j = i*2+1, 2, -1
+            new_E = new_E / j
+        enddo
+
+        sum = sum + new_E
+        if (new_E == old_E) exit
     enddo
 
     q = sum
-    
+
 end function q
-
-pure integer function fac(number)
-    implicit none
-
-    integer, intent(in) :: number
-    integer             :: n, i
-
-    n = 1
-    do i = 1, number
-        n = n * i
-    enddo
-    fac = n
-
-end function fac
 
 end program ex_8_24
