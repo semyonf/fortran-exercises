796fd6ddc4159fcae07c5540d80413cc455da233
Semyon Fomin <fs@get8.ru>
2016-11-27 19:02:46 +0300

    Finish lab_1_2

--- lab_1_2/src/lab_1_2.f90
+++ lab_1_2/src/lab_1_2.f90
@@ -3,9 +3,8 @@ program lab_1_2
 
     implicit none
     integer, parameter          :: N_RECORDS = 15, L_NAME = 15, L_POSITION = 15
-    character(kind=CH_)         :: names(N_RECORDS, L_NAME), positions(N_RECORDS, L_POSITION)!, types(N_RECORDS, L_POSITION)
-    integer                     :: In, Out
-    ! logical                     :: repeated(N_RECORDS) = .false.
+    character(kind=CH_)         :: names(N_RECORDS, L_NAME), positions(N_RECORDS, L_POSITION), types(N_RECORDS, L_POSITION)
+    integer                     :: In, Out, occurrences(N_RECORDS, 1)
     character(*), parameter     :: input_file = "../data/input.txt", &
                                    output_file = "output.txt"
 
@@ -15,23 +14,66 @@ program lab_1_2
     ! Вывод списка сотрудников и количества профессий
     call WriteEmployeeList(output_file, names, positions)
 
-    ! ! Поиск повторившихся профессий
-    ! call ProcessPositions(positions, types)
+    ! Поиск повторившихся профессий
+    call ProcessPositions(positions, types, occurrences)
 
-    ! Вывод профессий и их повторений
-    ! call WritePositionsOccured(output_file, uniquePositions)
+    ! Вывод профессий и их вхождений
+    call WritePositionsOccured(output_file, types, occurrences)
 
 contains
 
-    ! ! Подпроцесс для поиска и подсчета повторений профессий
-    ! pure subroutine ProcessPositions(positions, types)
-    !     implicit none
+    ! Подпроцесс для поиска и подсчета повторений профессий
+    subroutine ProcessPositions(positions, types, occurrences)
+        implicit none
 
-    !     character(kind=CH_) positions(:,:), types(:,:)
-    !     intent(in)  positions
-    !     intent(out) types
+        character(kind=CH_) positions(:,:), types(:,:)
+        integer occurrences(:,:)
+        intent(in)  positions
+        intent(out) types, occurrences
 
-    ! end subroutine ProcessPositions
+        integer                          :: N_unique = 0, i = 0, duplicates = 0
+        character(kind=CH_), allocatable :: newPositions(:,:)
+        logical                          :: repeated(N_RECORDS) = .false.
+
+        newPositions = positions
+
+        do while (.not. all(repeated))
+            N_unique = N_unique + 1
+            types(N_unique, :) = newPositions(1, :)
+            occurrences(N_unique,1) = count([(all(types(N_unique,:) == positions(i,:)), i = 1, N_RECORDS)])
+            repeated = repeated .or. [(all(types(N_unique,:) == positions(i,:)), i = 1, N_RECORDS)]
+            do i = 1, N_RECORDS
+                if (repeated(i) .eqv. .true.) then
+                    duplicates = duplicates + 1
+                else
+                    newPositions(i - duplicates,:) = positions(i,:)
+                endif
+            enddo
+            duplicates = 0
+        enddo
+
+    end subroutine ProcessPositions
+
+    ! Подпроцесс для записи списка профессий
+    subroutine WritePositionsOccured(output_file, types, occurrences)
+        implicit none
+
+        character(*)        output_file
+        character(kind=CH_) types(:,:)
+        integer             occurrences(:, :)
+        intent(in)  output_file, types, occurrences
+
+        integer :: Out, IO, i
+        character(:), allocatable :: format
+
+        format = '(' // L_POSITION // 'a1, I3)'
+
+        open (file=output_file, encoding=E_, newunit=Out)
+            write(Out, format, iostat=IO) (types(i,:), occurrences(i,1), i = 1, count(occurrences /= 0))
+            call Handle_IO_status(IO, 'WritePositionsOccured')
+        close (Out)
+
+    end subroutine WritePositionsOccured
 
     ! Подпроцесс для чтения списка сотрудников
     subroutine ReadEmployeeList(input_file, names, positions)
@@ -60,11 +102,10 @@ contains
 
         character(*) output_file
         character(kind=CH_) names(:,:), positions(:,:)
-        intent(in)  output_file
-        intent(out) names, positions
+        intent(in)  output_file, names, positions
 
         integer :: Out, IO, i
-        character(:), allocatable   ::format
+        character(:), allocatable ::format
 
         format = '(' // L_NAME // 'a1, ' // L_POSITION // 'a1)'
 
