be4d3a3003f1b042532959265b4bcf0a44e5b5ee
Semyon Fomin <fs@get8.ru>
2016-11-30 01:16:17 +0300

    Updated code style

--- lab_1_3/src/emp_process.f90
+++ lab_1_3/src/emp_process.f90
@@ -1,45 +1,45 @@
 module Emp_Process
-    use Environment
-    use Emp_IO
-    implicit none
+   use Environment
+   use Emp_IO
+   implicit none
 
 contains
 
 ! Подпроцесс для поиска и подсчета повторений профессий
-    pure subroutine ProcessPositions(employees, types, occurrences)
-        implicit none
+   pure subroutine ProcessPositions(employees, types, occurrences)
+      implicit none
 
-        type(employee) :: employees(:), types(:)
-        integer        :: occurrences(:)
-        intent(in) employees
-        intent(out) types, occurrences
+      type(employee) :: employees(:), types(:)
+      integer        :: occurrences(:)
+      intent(in) employees
+      intent(out) types, occurrences
 
-        integer                     :: N_unique, i, duplicates, records
-        type(employee), allocatable :: filtered(:)
-        logical, allocatable        :: repeated(:)
+      integer                     :: nth, i, duplicates, records
+      type(employee), allocatable :: filtered(:)
+      logical, allocatable        :: repeated(:)
 
-        records = size(employees(:))
-        allocate(repeated(records))
+      records = size(employees(:))
+      allocate(repeated(records))
 
-        N_unique = 1
-        duplicates = 0
-        repeated = .false.
-        filtered = employees
+      nth = 1
+      duplicates = 0
+      repeated = .false.
+      filtered = employees
 
-        do while (.not. all(repeated))
-            N_unique = N_unique + 1
-            types(N_unique)%position = filtered(1)%position
-            occurrences(N_unique) = count(types(N_unique)%position == employees(:)%position)
-            repeated = repeated .or. types(N_unique)%position == employees(:)%position
-            do i = 1, records
-                if (repeated(i) .eqv. .true.) then
-                    duplicates = duplicates + 1
-                else
-                    filtered(i - duplicates)%position = employees(i)%position
-                endif
-            enddo
-            duplicates = 0
-        enddo
-    end subroutine ProcessPositions
+      do while (.not. all(repeated))
+         nth = nth + 1
+         types(nth)%position = filtered(1)%position
+         occurrences(nth) = count(types(nth)%position == employees(:)%position)
+         repeated = repeated .or. types(nth)%position == employees(:)%position
+         do i = 1, records
+            if (repeated(i) .eqv. .true.) then
+               duplicates = duplicates + 1
+            else
+               filtered(i - duplicates)%position = employees(i)%position
+            endif
+         enddo
+         duplicates = 0
+      enddo
+   end subroutine ProcessPositions
 
 end module Emp_Process
