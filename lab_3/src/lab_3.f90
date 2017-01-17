286aeca235d4c05a758cd847ef7199c202d62a6e
Semyon Fomin <fs@get8.ru>
2017-01-17 23:24:20 +0300

    Configure stuf

--- lab_3/src/lab_3.f90
+++ lab_3/src/lab_3.f90
@@ -5,23 +5,28 @@ program lab_3
 
    implicit none
 
-   character(:), allocatable :: F1, F2!, F3
+   character(:), allocatable :: F1, F2, F3
 
    type(TextLine), pointer :: Original => Null(), & ! Оригинальный текст
-                              Normal   => Null()!, & ! Естественный порядок строк
-                              ! Sorted   => Null()    ! Сортированные строки
+                              Normal   => Null(), & ! Естественный порядок строк
+                              Sorted   => Null()    ! Сортированные строки
 
    ! Входной текстовый файл
    F1 = "../data/F1.txt"
    ! Текстовый файл в естественном порядке строк
-   F2 = "../out/F2.txt"
-   ! ! Текстовый файл в отсортированном порядке строк
-   ! F3 = "../out/F3.txt"
+   F2 = "F2.txt"
+   ! Текстовый файл в отсортированном порядке строк
+   F3 = "F3.txt"
 
    Original => Read_Text(F1)
 
-   if (Associated(Normal)) &
-      call Output_Source_Code(F2, Normal)
+   ! Временно
+   Normal => Original
+   Sorted => Original
 
+   if (Associated(Original)) then
+      call Output_Source_Code(F2, Normal)
+      call Output_Source_Code(F3, Sorted)
+   endif
 
 end program lab_3
