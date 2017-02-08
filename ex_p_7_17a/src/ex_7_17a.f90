4c98d19bd2da2238ac3a808058a2e4194e19deae
Semyon Fomin <fs@get8.ru>
2017-02-08 23:49:13 +0300

    Finish ex_p_7_17a

--- ex_p_7_17a/src/ex_7_17a.f90
+++ ex_p_7_17a/src/ex_7_17a.f90
@@ -3,8 +3,9 @@
 program ex_7_17a
     implicit none
 
-    integer, allocatable    :: A(:,:)
-    integer                 :: In, Out, x, y, i
+    integer, allocatable    :: A(:,:), Inline(:), Indexes(:,:), k(:)
+    logical, allocatable    :: mask(:)
+    integer                 :: In, Out, x, y, i, max, t, j, pos
 
     character(*), parameter :: output_file = "output.txt", &
                                input_file = "../data/input.txt"
@@ -12,12 +13,37 @@ program ex_7_17a
     open (file=input_file, newunit=In)
         read(In, *) x, y
         allocate(A(x,y))
-        read(In, *) (A(i,:), i = 1, y)
+        read(In, *) (A(:,i), i = 1, y)
     close (In)
 
+    max = maxval(A)
+
+    Inline = reshape(A, [x*y])
+    mask = Inline == max
+
+    pos = count(mask)
+
+    k = pack([(i,i=1,x*y)], mask)
+    allocate(Indexes(pos,2))
+
+    do t = 1, pos
+
+        i = mod(k(t),x)
+        j = k(t) / x
+
+        if (i == 0) then
+            i = x
+        else
+            j = j + 1
+        endif
+
+        Indexes(t,:) = [i, j]
+    enddo
+
     open (file=output_file, newunit=Out)
-        write(Out, *) (A(i,:), i = 1, y)
-        write(Out, *) 'Наибольший элемент:', maxval(A)
+        write(Out, *) 'Наибольший элемент:', max
+        write(Out, *) 'X, Y координаты элемента(ов)'
+        write(Out, '(I2,A1,I2)') (Indexes(i,1), ',' ,Indexes(i,2), i = 1, pos)
     close (Out)
 
 end program ex_7_17a
