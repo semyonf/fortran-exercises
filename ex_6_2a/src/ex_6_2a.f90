e38ebaf6e00809663b3acc5670d7b6924e2f8aa6
Semyon Fomin <fs@get8.ru>
2017-02-13 00:57:01 +0300

    Fix ex_6_2a

--- ex_6_2a/src/ex_6_2a.f90
+++ ex_6_2a/src/ex_6_2a.f90
@@ -1,43 +1,41 @@
 ! 6.2a в учебнике
 
 program ex_6_2a
-    implicit none
+   implicit none
 
-    integer, parameter      :: R_ = 16
-    real, parameter         :: PI = 3.141592653
-    real(R_)                :: x = 0, numerator = 0, currentElement = 0, currentSum = 0, newSum = 0
-    integer                 :: In = 0, Out = 0, denominator = 0, i = 0, denominator_fact = 0, denominator_mul = 0
-    character(*), parameter :: output_file = "output.txt", &
-                               input_file = "../data/input.txt"
+   integer, parameter      :: R_ = 16
+   real(R_), parameter         :: PI = 4*ATAN(1.0)
+   real(R_)                :: x = 0, numerator = 0, currentElement = 0, currentSum = 0, newSum = 0, numeratorDiff = 0
+   integer                 :: In = 0, Out = 0, denominator = 0, i = 0, denominator_fact = 0, denominator_mul = 0
+   character(*), parameter :: output_file = "output.txt", input_file = "../data/input.txt"
 
-    open (file=input_file, newunit=In)
-        read(In,*) x
-    close (In)
+   open (file=input_file, newunit=In)
+      read(In,*) x
+   close (In)
 
-    currentElement   = 1
-    numerator        = 1
-    denominator      = 1
-    denominator_fact = 1
+   currentElement   = 1
+   numerator        = 1
+   denominator      = 1
+   denominator_fact = 1
+   numeratorDiff    = x**2
 
-    do
-        i = i + 1
-        newSum = currentSum + currentElement
-        if (currentSum == newSum .or. i > 12) exit
+   do while (denominator > 0)
+      i = i + 1
 
-        currentSum = newSum
+      newSum = currentSum + currentElement
+      currentSum = newSum
 
-        numerator = numerator * x**2
+      numerator = - numerator * numeratorDiff
 
-        denominator_fact = denominator_fact * i
-        denominator_mul = (2 * i) + 1
-        denominator = denominator_fact * denominator_mul
+      denominator_fact = denominator_fact * i
+      denominator_mul = (2 * i) + 1
+      denominator = denominator_fact * denominator_mul
 
-        currentElement = numerator / denominator
-        if (mod(i,2) == 1) currentElement = -currentElement
-    end do
+      currentElement = numerator / denominator
+   enddo
 
-    open (file=output_file, newunit=Out)
-        write(Out, '(f5.2,a,f5.2)') (2 * x) / sqrt(PI) * currentSum, ' ~' , erf(x)
-    close (Out)
+   open (file=output_file, newunit=Out)
+      write(Out, '(f5.2,a,f5.2)') (2 * x) / sqrt(PI) * currentSum, ' ~' , erf(x)
+   close (Out)
 
 end program ex_6_2a
