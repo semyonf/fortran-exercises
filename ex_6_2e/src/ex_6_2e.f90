ee2ed7ac1c87d628798969484c0d716a261bc8b4
Semyon Fomin <fs@get8.ru>
2017-02-13 01:23:00 +0300

    Fix ex_6_2e

--- ex_6_2e/src/ex_6_2e.f90
+++ ex_6_2e/src/ex_6_2e.f90
@@ -1,32 +1,34 @@
 ! 6.2e в учебнике
 
 program ex_6_2e
-    implicit none
+   implicit none
 
-    integer, parameter      :: R_ = 16
-    character(*), parameter :: output_file = "output.txt", &
-                               input_file = "../data/input.txt"
-    real(R_)                :: x = 0, a = 0, currentElement = 0, currentSum = 0, newSum = 0, numerator = 0
-    integer                 :: In = 0, Out = 0, denominator = 0
+   integer, parameter      :: R_ = 16
+   real(R_)                :: x = 0, a = 0, currentElement = 0, currentSum = 0, newSum = 0, numeratorDiff = 0
+   integer                 :: In = 0, Out = 0, i = 0
+   character(*), parameter :: output_file = "output.txt", input_file = "../data/input.txt"
 
-    open (file=input_file, newunit=In)
-        read(In,'(f5.2)') a
-        read(In,'(f5.2)') x
-    close (In)
+   open (file=input_file, newunit=In)
+      read(In,'(f5.2)') a
+      read(In,'(f5.2)') x
+   close (In)
 
-    currentElement = 1
+   currentElement = 1
+   newSum = 1
+   numeratorDiff = x * log(a)
 
-    do
-        currentSum = newSum
-        newSum = currentSum + currentElement
-        numerator = x * log(a)
-        denominator = denominator + 1
-        currentElement = currentElement * numerator / denominator
-        if (currentSum == newSum) exit
-    end do
+   do
+      i = i + 1
+      currentSum = newSum
 
-    open (file=output_file, newunit=Out)
-        write(Out, '(f5.2,a,f5.2)') currentSum, ' ~ ' , a**x
-    close (Out)
+      currentElement = currentElement * numeratorDiff / i
+
+      newSum = currentSum + currentElement
+      if (currentSum == newSum) exit
+   end do
+
+   open (file=output_file, newunit=Out)
+      write(Out, '(f5.2,a,f5.2)') currentSum, ' ~ ' , a**x
+   close (Out)
 
 end program ex_6_2e
